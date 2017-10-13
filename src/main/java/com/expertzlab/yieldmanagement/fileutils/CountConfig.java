8eda1ab258e6510b237a8e781c2e5bad5452c309
Gireesh Babu <gireesh.babu@nibodha.com>
2017-10-13 18:45:45 +0530

    count configured

--- src/main/java/com/expertzlab/yieldmanagement/fileutils/CountConfig.java
+++ src/main/java/com/expertzlab/yieldmanagement/fileutils/CountConfig.java
@@ -5,9 +5,9 @@ package com.expertzlab.yieldmanagement.fileutils;
  */
 public class CountConfig {
 
-    public static final int PROPERTY_MANAGER_COUNT = 10;
-    public static final int OWNER_COUNT = 10;
-    public static final int OWNER_PROPERTY_COUNT = 5;
-    public static final int COMPETING_PROPERTY_COUNT = 5;
+    public static final int PROPERTY_MANAGER_COUNT = 100000;
+    public static final int OWNER_COUNT = 500;
+    public static final int OWNER_PROPERTY_COUNT = 100;
+    public static final int COMPETING_PROPERTY_COUNT = 50;
 
 }
